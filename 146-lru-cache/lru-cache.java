79b63c1f58b6c757e1e68d942dec360fa2477197
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-08-04 20:11:36 +0530

    Time: 48 ms (51.73%) | Memory: 114.1 MB (46.80%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,38 +1,83 @@
-class LRUCache {
+    class Node {
+        public int key;
+        public int val;
+        public Node next;
+        public Node prev;
 
-    int len;
-    Map<Integer , Integer> map;
-    Queue<Integer> q;
-    public LRUCache(int capacity) {
-        map = new HashMap<>();
-        q = new LinkedList<>();
-        len = capacity;
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+            next = null;
+            prev = null;
+        }
     }
-    
+
+class LRUCache {
+      private Map<Integer , Node> map;      //Will store the address of the Node to get the element in O(1)
+        private Node head;
+        private Node tail;
+        private int size;
+    public LRUCache(int capacity) {
+      size = capacity;
+      map = new HashMap<>();
+      head = new Node(-1 , -1);
+    tail = new Node(-1 , -1);
+    head.next = tail;
+    tail.prev = head;
+
+    }
+
+    private void deleteNode(Node p){
+        Node pre = p.prev;
+        Node nex = p.next;
+        pre.next = nex;
+        nex.prev= pre;
+    }
+
+    private void addNode(Node newNode){
+        Node temp = head.next;
+        head.next = newNode;
+        newNode.prev = head;
+        newNode.next = temp;
+        temp.prev = newNode;
+    }
+
     public int get(int key) {
         if(!map.containsKey(key)){
             return -1;
         }
-        q.remove(key);
-        q.add(key);
-        return map.get(key);
-        
+        Node p = map.get(key);
+        deleteNode(p);
+        addNode(p);
+        map.put(key , head.next);
+        return head.next.val;
+
     }
-    
+
     public void put(int key, int value) {
+
         if(map.containsKey(key)){
-            q.remove(key);
-            q.add(key);
-            map.put(key , value);
+            Node c = map.get(key);
+            deleteNode(c);          //Remove the old version if it already exists 
+            c.val = value;          //update the value
+            addNode(c);             //ADd the node so we do noot change the Least Frequently Used Mechanism
+            map.put(key , head.next);
         }
         else{
-            if(q.size() == len){
-                int k = q.poll();
-                map.remove(k);
+            if(map.size() == size){
+                Node prev = tail.prev;
+                deleteNode(prev);
+                Node l = new Node(key , value);
+                addNode(l);
+                map.remove(prev.key);
+                map.put(key , head.next);
+            }else{
+                Node l = new Node(key , value);
+                addNode(l);
+                map.put(key , head.next);
             }
-            q.add(key);
-            map.put(key , value);
         }
+
     }
 }
 
