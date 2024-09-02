7230cb4a509a15895c13a46a6f96d627296c48fd
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-09-02 23:26:44 +0530

    Time: 34 ms (56.73%) | Memory: 62.6 MB (48.43%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,15 +1,18 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        long evenSum = 0; // Maximum sum when ending at an even index
-        long oddSum = 0;  // Maximum sum when ending at an odd index
-        
-        for (int num : nums) {
-            long newEvenSum = Math.max(evenSum, oddSum + num);
-            long newOddSum = Math.max(oddSum, evenSum - num);
-            evenSum = newEvenSum;
-            oddSum = newOddSum;
+        int n = nums.length;
+
+        long[][] t = new long[n][2];
+        // 0 is for even
+        // 1 is for odd
+        t[0][0] = Math.max(-nums[0], 0);
+        t[0][1] = Math.max(nums[0], 0);
+
+        for (int i = 1; i < n; i++) {
+            t[i][0] = Math.max(t[i - 1][1] - nums[i], t[i - 1][0]);
+            t[i][1] = Math.max(t[i - 1][0] + nums[i], t[i - 1][1]);
         }
-        
-        return evenSum;
+
+        return Math.max(t[n - 1][0], t[n - 1][1]);
     }
 }
