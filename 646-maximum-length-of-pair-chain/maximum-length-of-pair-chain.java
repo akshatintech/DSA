2639e01972f5130f66378120bbf3f78bc753c9d5
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-09-12 16:50:09 +0530

    Time: 35 ms (17.42%) | Memory: 44.8 MB (72.68%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int findLongestChain(int[][] pairs) {
-        int len = pairs.length;
-        Arrays.sort(pairs, (a, b) -> (a[1] - b[1]));
-        int[][] dp = new int[len][2002];
-        return solve(pairs, 0, len, -1001, dp);
-    }
-    private int solve(int[][] pairs, int index, int len, int prev, int[][] dp)
-    {
-        if(index >= len)
-            return 0;
-        if(dp[index][prev + 1001] != 0)
-            return dp[index][prev + 1001];
-        int include = 0;
-        if(pairs[index][0] > prev)
-            include =  1 + solve(pairs, index + 1, len, pairs[index][1], dp);
-        int exclude = solve(pairs, index + 1, len, prev, dp);
-        return dp[index][prev + 1001] = Math.max(include, exclude);
+    public int findLongestChain(int[][] ps) {
+        Arrays.sort(ps, (a, b) -> a[0]-b[0] );
+
+        int n = ps.length, nxt[] = new int[n+1];
+    
+        for(int ind=n-1; ind>=0; ind--){
+            for(int prevInd = ind; prevInd>=0; prevInd--){
+                int ans = 0, prevVal = ( prevInd == 0 ? Integer.MIN_VALUE : ps[prevInd-1][1] );
+
+                if( prevVal < ps[ind][0] ) ans = 1 + nxt[ind+1];
+                ans = Math.max(ans, nxt[prevInd] );
+
+                nxt[prevInd] = ans;
+            }
+        }
+        return nxt[0];
     }
 }
