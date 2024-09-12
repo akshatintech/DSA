07af5c00f599381aa3373110dd67719c1a7812b5
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-09-12 20:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (35.32%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,26 @@
 class Solution {
+    public int[] t  = new int[101];
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n ==1){
-            return nums[0];
-        }
-
-        int[] t = new int[n+1];
-        // t[i] = max profit till house i
-
-        t[0] = 0;
-        t[1] = nums[0];
-
-        for(int i = 2 ;i <= n ;i++){
-            int skip = t[i-1];
-            int steal = nums[i-1] + t[i-2];
-
-            t[i] = Math.max(skip  , steal);
-        }
-
-        return t[n];
+        Arrays.fill(t , -1);
+        return solve(nums , 0 , n);
     }
+
+    public int solve(int[] nums , int index , int n){
+        if(index >=n){
+            return 0;
+        }
+
+
+        if(t[index] != -1){
+            return t[index];
+        }
+
+        int take = nums[index] + solve(nums , index +2 , n);
+        int skip = solve(nums , index+1 , n);
+
+        return t[index] = Math.max(take, skip);
+
+    }
+
 }
