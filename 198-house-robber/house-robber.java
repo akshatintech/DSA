e7ddc6aed0e64dfbb3e3441233dbb19e1bc7f27d
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-09-12 21:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (25.24%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -4,14 +4,21 @@ class Solution {
         int[] dp = new int[n+1];
         dp[0] = 0;
         dp[1] = nums[0];
-
+        int prev = nums[0];
+        int prevPrev = 0;
+        int ans = 0;
         for(int i = 2 ; i <= nums.length ; i++){
-            int skip = dp[i-1];
-            int take = nums[i-1]+ dp[i-2];
 
-            dp[i] = Math.max(skip , take);
+
+
+            int skip = prev;
+            int take = nums[i-1] + prevPrev;
+
+            ans = Math.max(skip , take);
+            prevPrev = prev;
+            prev = ans;
         }
 
-        return dp[n];
+        return prev;
     }
 }
