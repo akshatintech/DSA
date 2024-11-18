233a35a448b53007bbbac7a21c0b72331bb0ddce
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-11-18 19:59:54 +0530

    Time: 1 ms (65.32%) | Memory: 45.6 MB (92.16%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,31 +1,26 @@
 class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
-        int trappedWater =0;
+        int trappedWater = 0;
+
         int[] leftMax = new int[n];
         leftMax[0] = arr[0];
-
-        for(int i = 1 ;i < n ;i++){
-            leftMax[i] = Math.max(arr[i] , leftMax[i-1]);
+        for(int i = 1; i < n ;i++){
+            leftMax[i] = Math.max(leftMax[i-1] , arr[i]);
         }
-
 
         int[] rightMax = new int[n];
         rightMax[n-1] = arr[n-1];
-
-        for(int i = n-2 ;i >= 0 ;i--){
-            rightMax[i] = Math.max(arr[i] , rightMax[i+1]);
+        for(int i = n-2 ;i >=0 ;i--){
+            rightMax[i] = Math.max(rightMax[i+1] , arr[i]);
         }
 
 
-        //Calculating the rain water   
-        int trapperWater = 0;
-
-        for(int i = 0; i < n ;i++) {
-
+        for(int i = 0 ;i < n ; i++){
             int waterLevel = Math.min(leftMax[i] , rightMax[i]);
+            
+            trappedWater = trappedWater + waterLevel -arr[i];
 
-            trappedWater = trappedWater + waterLevel - arr[i];
         }
 
         return trappedWater;
