7752e7d5ed351665655278c019cc5db9f0542a5e
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-11-21 20:28:42 +0530

    Time: 24 ms (90.31%) | Memory: 51.6 MB (33.08%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,41 +1,44 @@
-
 class Solution {
     public int sumSubarrayMins(int[] arr) {
         int length = arr.length;
-
-        Stack<Integer> stack = new Stack<>();
-
-        // Arrays to store 
         int[] left = new int[length];
         int[] right = new int[length];
-        int mod = (int) 1e9 + 7;
-
-        // Fill the left array with previous less elements
-        for (int i = 0; i < length ; i++) {
+      
+        Arrays.fill(left, -1);
+        Arrays.fill(right, length);
+      
+        Deque<Integer> stack = new ArrayDeque<>();
+      
+        for (int i = 0; i < length; ++i) {
             while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
                 stack.pop();
             }
-            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            if (!stack.isEmpty()) {
+                left[i] = stack.peek();
+            }
             stack.push(i);
         }
+      
         stack.clear();
-
-        // Fill the right array with next less elements
-        for (int i = length - 1; i >= 0; i--) {
+      
+        for (int i = length - 1; i >= 0; --i) {
             while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
                 stack.pop();
             }
-            right[i] = stack.isEmpty() ? length : stack.peek();
+            if (!stack.isEmpty()) {
+                right[i] = stack.peek();
+            }
             stack.push(i);
         }
-
-        long ans = 0;
+      
+        int mod = (int) 1e9 + 7;
+        long answer = 0;
+      
         for (int i = 0; i < length; ++i) {
-            // Number of subarrays in which arr[i] is the minimum
-            long cntLeft = i - left[i];
-            long cntRight = right[i] - i;
-            ans = (ans + (cntLeft * cntRight % mod) * arr[i] % mod) % mod;
+            answer += (long) (i - left[i]) * (right[i] - i) % mod * arr[i] % mod;
+            answer %= mod;
         }
-        return (int) ans;
+      
+        return (int) answer;
     }
 }
