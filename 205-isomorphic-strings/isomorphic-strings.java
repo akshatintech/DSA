3ae8f068561eac9ff66fcaf36c03b872a9838ab4
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-12-16 19:04:44 +0530

    Time: 17 ms (38.10%) | Memory: 43 MB (8.50%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,37 +1,37 @@
-class Solution {
+import java.util.HashMap;
+
+public class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
-		int[] indexS = new int[200];		//Stroes
-		int[] indexT = new int[200];
-		
-		
-		int len = s.length();
-		
-		if(len != t.length()) {
-			return false;
-		}
-		
-		//iterate through each character 
-		for(int i = 0;i<len;i++) {
-			//check the index of current character in string s
-			// is different from the corresonding charcter 
-			if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
-				return false;
-			}
-			
-			
-			//Update the indices of the string
-			indexS[s.charAt(i)] = i+1;
-			indexT[t.charAt(i)] = i+1;
-			
-		}
-		
-		
-		return true;
-		
-		
-		
-	
-        
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> mapS = new HashMap<>();
+        HashMap<Character, Character> mapT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            // Check mapping from s to t
+            if (mapS.containsKey(charS)) {
+                if (mapS.get(charS) != charT) {
+                    return false;
+                }
+            } else {
+                mapS.put(charS, charT);
+            }
+
+            // Check mapping from t to s
+            if (mapT.containsKey(charT)) {
+                if (mapT.get(charT) != charS) {
+                    return false;
+                }
+            } else {
+                mapT.put(charT, charS);
+            }
+        }
+
+        return true;
     }
 }
