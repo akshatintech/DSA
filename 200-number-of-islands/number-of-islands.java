a4dd71e57323edba5417a6a311108b851378daab
akshatintech <145540776+akshatintech@users.noreply.github.com>
2024-12-20 23:45:09 +0530

    Time: 6 ms (25.02%) | Memory: 52.1 MB (19.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,7 +1,8 @@
+
 class Pair{
     int f;
     int s;
-    public Pair(int first,int second){
+    public Pair(int first , int second){
         this.f = first;
         this.s = second;
     }
@@ -12,39 +13,42 @@ class Solution {
         int m = grid[0].length;
 
         int[][] vis = new int[n][m];
+
         int count = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i = 0;i < n  ;i++){
+            for(int j = 0; j < m ;j++){
                 if(vis[i][j] == 0 && grid[i][j] == '1'){
-                    bfs(i,j,vis,grid);
+                    bfs(i , j , vis , grid);
                     count++;
                 }
             }
         }
+
         return count;
     }
-    public void bfs(int row,int col,int[][] vis,char[][] grid){
+
+    public void bfs(int row ,int col , int[][] vis  , char[][] grid){
         vis[row][col] = 1;
 
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(row,col));
-        int n = grid.length,m = grid[0].length;
+        Queue<Pair> q =  new LinkedList<Pair>();
+        q.add(new Pair(row , col));
+        int n = grid.length , m = grid[0].length;
 
         while(!q.isEmpty()){
             int r = q.peek().f;
             int c = q.peek().s;
             q.poll();
 
-            for(int drow=-1; drow<=1; drow++){
-                for(int dcol=-1; dcol<=1; dcol++){
+            for(int drow = -1;drow <= 1 ;drow++){
+                for(int dcol = -1 ; dcol <= 1;dcol++){
                     int nrow = r + drow;
                     int ncol = c + dcol;
 
-                    if((Math.abs(drow-dcol) == 1) && (nrow >= 0 && nrow < n) && (ncol >= 0 && ncol < m) && 
-                        vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
+                    if((Math.abs(drow -dcol) == 1 ) &&(nrow >= 0 &&nrow < n) &&(ncol >=0 &&ncol < m)&&
+                    vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
                         vis[nrow][ncol] = 1;
-                        q.add(new Pair(nrow,ncol));
+                        q.add(new Pair(nrow , ncol));
                     }
                 }
             }
