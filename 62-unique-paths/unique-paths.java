c771854fda6c1bbe2c7aa2ab0fb52f0a37f18597
akshatintech <145540776+akshatintech@users.noreply.github.com>
2025-01-20 20:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (10.00%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,33 +1,33 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-         int[][] dp = new int[m+1][n+1];
-         for(int[] arr : dp){
+        int[][] dp = new int[m+1][n+1];
+        for(int[] arr : dp){
             Arrays.fill(arr , -1);
-         }
-
-         boolean[][] vis = new boolean[m][n];
-         return fun(m-1 , n-1 , vis , dp);
+        }
+        boolean[][] vis = new boolean[m][n];
+        return fun(m-1 , n-1 , dp , vis);
     }
 
-    public int fun(int i , int j , boolean[][] vis , int[][] dp){
+    public int fun(int i , int j , int[][] dp , boolean[][] vis){
         if(i < 0 || j < 0 || vis[i][j] == true){
             return 0;
         }
+
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        if(i == 0 && j == 0){
+
+        if(i ==0 && j==0){
             return 1;
         }
 
         int ans = 0;
         if(!vis[i][j]){
-            vis[i][j]= true;
-            ans = fun(i-1 , j , vis , dp) + fun(i , j-1 , vis , dp);
+            vis[i][j] = true;
+            ans = fun(i-1 , j , dp , vis) + fun(i , j-1 , dp , vis);
             vis[i][j] = false;
         }
 
         return dp[i][j] = ans;
     }
-    
 }
