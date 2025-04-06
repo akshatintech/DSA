070c8d3ed3b377d5715bb44bc5c23d3fd7259b50
akshatintech <145540776+akshatintech@users.noreply.github.com>
2025-04-06 19:17:19 +0530

    Time: 14 ms (26.02%) | Memory: 60.4 MB (7.56%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,30 +1,18 @@
 class Solution {
-    // Function to swap elements in the array
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        
-        // Place each positive integer i at index i-1 if possible
-        for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
-                swap(nums, i, nums[i] - 1);
+        int min =nums[0];
+           HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i < nums.length;i++){
+            set.add(nums[i]);
+            if(nums[i] < min && nums[i] >0){
+                min = nums[i];
             }
         }
-        
-        // Find the first missing positive integer
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
-            }
+     
+        int num=1;
+        while(set.contains(num)){
+            num++;
         }
-        
-        // If all positive integers from 1 to n are present, return n + 1
-        return n + 1;
+        return num;
     }
 }
-
