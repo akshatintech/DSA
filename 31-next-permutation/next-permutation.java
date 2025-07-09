6ab25d56ffaf62f936510df6980dcfa066a10c9b
akshatintech <145540776+akshatintech@users.noreply.github.com>
2025-07-09 16:29:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (21.31%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,52 +1,45 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        int n = nums.length;
 
-        int n= nums.length;
-
-        int idx = -1;       //break point
-
-        for(int i = n-2 ;i >=0 ;i--){
+        int pivot = -1;
+        for(int i = n -2;i >=0;i--){
             if(nums[i] < nums[i+1]){
-                idx = i;
-                break;              //idx  = i
+                pivot = i;
+                break;
             }
         }
+        
 
-
-        //if we do not get the break point reverse the array
-        if(idx == -1){
-            rev(nums , 0 , n-1);
+        if(pivot == -1){
+            reverse(nums , 0 , n-1);
             return;
         }
 
-        //swap the index with the next greater element
-
-        for(int i = n-1 ;i > idx ;i--){
-            if(nums[i] > nums[idx]){
-                int temp = nums[i];
-                nums[i] = nums[idx];
-                nums[idx] =temp;
+        //Find the next greater element
+        for(int i=n-1;i > pivot ;i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums , i , pivot);
                 break;
             }
         }
 
-        rev(nums , idx+1 , n-1);
-        return;
-        
+        reverse(nums , pivot+1 , n-1);
+
+
+
     }
 
-
-    private void rev(int[] arr , int low , int high){
-
-        int i = low;
-        int j = high;
-
-        while(i < j){
-            int temp = arr[i];
-            arr[i]=  arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+    public void reverse(int[] nums , int start , int end){
+        while(start < end){
+            swap(nums , start++ , end--);
         }
+    }
+
+    public void swap(int[] nums , int start , int end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        
     }
 }
