31482780eb63a9c5aa9812dcbdad7d7984338376
akshatintech <145540776+akshatintech@users.noreply.github.com>
2025-08-01 00:07:45 +0530

    Time: 1 ms (77.80%) | Memory: 46.1 MB (36.57%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int totalProfit = 0;
+        int profit = 0;
 
         for (int i = 1; i < prices.length; i++) {
-            // If selling today is profitable compared to buying yesterday
+            // If price today is greater than yesterday, take the profit
             if (prices[i] > prices[i - 1]) {
-                totalProfit += prices[i] - prices[i - 1];
+                profit += prices[i] - prices[i - 1];
             }
         }
 
-        return totalProfit;
+        return profit;
+        
     }
 }
