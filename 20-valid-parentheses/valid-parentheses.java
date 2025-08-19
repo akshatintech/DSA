d2f80c00093f48dba9a17e6bbf40428508ef8113
akshatintech <145540776+akshatintech@users.noreply.github.com>
2025-08-19 08:02:25 +0530

    Time: 2 ms (96.56%) | Memory: 41.6 MB (82.54%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,28 +1,43 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
+        Stack<Character> st= new Stack<>();
+        for(int i = 0;i < s.length() ;i++){
             char ch = s.charAt(i);
-
-            if (ch == '(' || ch == '{' || ch == '[') {
+            char temp;
+            if(ch == '(' || ch == '[' || ch == '{'){
                 st.push(ch);
-            } else if (!st.isEmpty()) {
-                char cha = st.peek();
-                if ((ch == '}' && cha != '{') ||
-                    (ch == ']' && cha != '[') ||
-                    (ch == ')' && cha != '(')) {
+            }
+            else if(!st.isEmpty()){
+                 if(ch ==')'){
+                temp = st.pop();
+                
+                if(temp != '('){
                     return false;
-                } else {
-                    st.pop();
                 }
-            } else {
-                // If stack is empty and there is a closing bracket, it's invalid
+            }
+             else if(ch =='}'){
+                if(!st.isEmpty()){}
+                temp = st.pop();
+                if(temp != '{'){
+                    return false;
+                }
+            }
+             else if(ch ==']'){
+                temp = st.pop();
+                if(temp != '['){
+                    return false;
+                }
+            }
+
+            }
+            else if(st.isEmpty()){
                 return false;
             }
+            
         }
-
-        // The stack should be empty if all brackets were matched
         return st.isEmpty();
+        
+        
     }
 }
